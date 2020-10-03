7f15d5ff5b125d6cbd49b7133ddc2584253b5e2a
Corey Hunter <corey@Coreys-MacBook-Air.local>
2020-10-03 16:08:44 -0700

    change port to run as non-root

--- looking-glass.go
+++ looking-glass.go
@@ -18,7 +18,7 @@ func main() {
 	router.Methods("GET").HandlerFunc(traceUI)
 	router.Methods("POST").HandlerFunc(doTrace)
 	server := &http.Server{
-		Addr:    ":80",
+		Addr:    ":3000",
 		Handler: router,
 	}
 
